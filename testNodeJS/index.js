0dd9df7dcaf3c053680a75ab15b0477574860005
27ugeen <27ugeen@gmail.com>
2020-07-31 23:20:03 +0300

    nodejs with docker done

---
+++ testNodeJS/index.js
@@ -0,0 +1,16 @@
+// берём Express
+const express = require('express');
+
+// создаём Express-приложение
+const app = express();
+
+// создаём маршрут для главной страницы
+// http://localhost:8080/
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
+
+// запускаем сервер на порту 8080
+app.listen(3000);
+// отправляем сообщение
+console.log('Сервер стартовал!');
